5a44be3b23803e9f95d28ba798ad81eb7b907f6a
Amit258012 <amit258012@gmail.com>
2023-10-11 13:56:41 +0530

    problem 3

---
+++ Day29/letter_combinations_number.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/group-anagrams/
+
+"""
+Time complexity:- O(n * 4^n)
+Space Complexity:- O(n)
+"""
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # Define a mapping of phone digits to corresponding letters.
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        res = []  # Initialize an empty list to store the combinations.
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                # If the current combination has the same length as the input digits, add it to the result.
+                res.append(curStr)
+                return
+
+            for ch in phone[digits[i]]:
+                # Recursively explore all possible combinations.
+                backtrack(i + 1, curStr + ch)
+
+        if digits:
+            backtrack(0, "")  # Start the backtracking process with an empty string.
+        return res
