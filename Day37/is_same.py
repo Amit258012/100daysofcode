16ad8b744bc31bac95b692ee3a24e57ff86f20f7
Amit258012 <amit258012@gmail.com>
2023-10-19 09:57:45 +0530

    problem 1

---
+++ Day37/is_same.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+
+"""
+Time complexity:- O(n)
+Space Complexity:- O(1)
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Check if both p and q are None (empty trees).
+        if not p and not q:
+            return True
+        # If one of p or q is None, or their values are not equal, the trees are not the same.
+        if not p or not q or p.val != q.val:
+            return False
+        # Recursively call isSameTree on the left and right subtrees of p and q,
+        # to check if they are the same.
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
