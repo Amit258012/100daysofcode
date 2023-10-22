2aa5ffa47a51b001942ce345fb79c354f5b6f950
Amit258012 <amit258012@gmail.com>
2023-10-22 21:12:39 +0530

    problem 1

---
+++ Day40/lowest_common_ancestor_bst.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+"""
+Time complexity:- O(H)
+Space Complexity:- O(1)
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import List, Optional
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        while True:
+            # If the current root value is less than both p and q, the LCA must be in the right subtree.
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            # If the current root value is greater than both p and q, the LCA must be in the left subtree.
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            # If the current root value is between p and q, or equal to either of them, it is the LCA.
+            else:
+                return root
