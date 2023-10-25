3eefcc15d9c63c32c8d9657760f2dd896c2165fb
Amit258012 <amit258012@gmail.com>
2023-10-25 21:34:44 +0530

    created class

--- Day43/binary_tree_to_linked_list.py
+++ Day43/binary_tree_to_linked_list.py
@@ -17,22 +17,23 @@ class TreeNode:
 from typing import Optional
 
 
-def flatten(self, root: Optional[TreeNode]) -> None:
-    """
-    Do not return anything, modify root in-place instead.
-    """
-    self.prevRight = None
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.prevRight = None
 
-    # Define a helper function named 'helper' for recursive processing.
-    def helper(root):
-        if root:
-            # Recursive call to process the right subtree first.
-            helper(root.right)
-            # Recursive call to process the left subtree.
-            helper(root.left)
-            # Reorder the tree nodes to make the current node the right child of 'self.prevRight'.
-            root.right, self.prevRight = self.prevRight, root
-            root.left = None
+        # Define a helper function named 'helper' for recursive processing.
+        def helper(root):
+            if root:
+                # Recursive call to process the right subtree first.
+                helper(root.right)
+                # Recursive call to process the left subtree.
+                helper(root.left)
+                # Reorder the tree nodes to make the current node the right child of 'self.prevRight'.
+                root.right, self.prevRight = self.prevRight, root
+                root.left = None
 
-    # Start the recursion from the root node.
-    helper(root)
+        # Start the recursion from the root node.
+        helper(root)
