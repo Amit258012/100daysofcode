72c361349ecede564c6ee60bf7adad47c3a2e791
Amit258012 <amit258012@gmail.com>
2023-10-29 15:25:13 +0530

    space optimize steps

--- Day47/kth_largest_node_bst.py
+++ Day47/kth_largest_node_bst.py
@@ -25,7 +25,7 @@ class Solution:
             if not node:
                 return
             inorder(node.left)
-            res.append(node.data)  # Collect elements in ascending order.
+            res.append(node.val)  # Collect elements in ascending order.
             inorder(node.right)
 
         inorder(root)  # Perform in-order traversal.
@@ -34,3 +34,9 @@ class Solution:
             return res[-k]  # Return the k-th largest element from the end of the list.
         else:
             return -1  # If k is greater than the number of elements, return -1.
+
+
+# space complexity can be optimized like this: O(1)
+# 1) First do 1 dfs traversal and calc total number or nodes (N)
+# 2) keep reference of count
+# 3) If count == k retun node value :- avoide storing the inorder in array
