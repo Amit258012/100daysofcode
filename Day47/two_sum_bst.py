373040159e46e51ba6ec224b4bf0d8d78c7d24f1
Amit258012 <amit258012@gmail.com>
2023-10-29 15:27:34 +0530

    two sum in bst

---
+++ Day47/two_sum_bst.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+
+"""
+Time complexity:- O(N)
+Space Complexity:- O(N) Auxilary space (Function CallStack)
+"""
+
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        # Create a dictionary 'vals' to store values encountered during traversal.
+        vals = {}
+
+        # Define a helper function for DFS.
+        def helper(node):
+            if not node:
+                return False
+            if node.val in vals:
+                return True  # If a complement exists, return True.
+
+            # Store the complement of the current node's value.
+            vals[k - node.val] = True
+
+            # Recursively search in the left and right subtrees.
+            return helper(node.left) or helper(node.right)
+
+        return helper(root)
