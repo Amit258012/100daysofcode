fd1d88210d0c1b8e7da889b86cfbec47080fa136
Amit258012 <amit258012@gmail.com>
2023-11-03 09:28:02 +0530

    problem 2

---
+++ Day52/sum_of_two_integers.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/sum-of-two-integers/
+
+"""
+Time complexity:- O(1)
+Space Complexity:- O(1) 
+"""
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF  # Initialize a mask to 32 bits (0xffffffff in hexadecimal).
+
+        while (b & mask) > 0:
+            carry = (
+                a & b
+            ) << 1  # Calculate the carry bit using bitwise AND and left shift.
+            a = a ^ b  # Calculate the sum of 'a' and 'b' using bitwise XOR.
+            b = carry  # Update 'b' with the carry bit.
+
+        return (a & mask) if b > 0 else a  # Return the result, considering overflow.
