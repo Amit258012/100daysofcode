31c00c5310258221eafb93533ef47823c958b5fd
Amit258012 <amit258012@gmail.com>
2023-11-07 19:31:18 +0530

    small typo

--- Day56/matrix_diagonal_sum.py
+++ Day56/matrix_diagonal_sum.py
@@ -12,17 +12,17 @@ from typing import List
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         n = len(mat)  # Get the size of the square matrix
-        diagonal_sum = 0  # Initialize the diagonal sum
+        diagonalSum = 0  # Initialize the diagonal sum
 
         for i in range(n):
-            diagonal_sum += mat[i][i]  # Add the elements of the primary diagonal
-            diagonal_sum += mat[i][
-                n - 1 - i
-            ]  # Add the elements of the secondary diagonal
+            # Add the elements of the primary diagonal
+            diagonalSum += mat[i][i]
+            # Add the elements of the secondary diagonal
+            diagonalSum += mat[i][n - 1 - i]
 
         # If the matrix has an odd size, subtract the center element to avoid double-counting
         if n % 2 == 1:
-            center_index = n // 2
-            diagonal_sum -= mat[center_index][center_index]
+            centerIndex = n // 2
+            diagonalSum -= mat[centerIndex][centerIndex]
 
-        return diagonal_sum  # Return the calculated diagonal sum
+        return diagonalSum  # Return the calculated diagonal sum
