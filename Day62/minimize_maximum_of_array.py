1ed9006b2a8ecea21b80272939fd4ba103b8c7d4
Amit258012 <amit258012@gmail.com>
2023-11-13 15:57:24 +0530

    problem 3

---
+++ Day62/minimize_maximum_of_array.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/minimize-maximum-of-array/
+
+"""
+Time complexity:- O(N)
+Space Complexity:- O(1) 
+"""
+
+"""
+Intuition:
+
+The code iterates through the array and calculates the prefix sum at each step.
+It calculates the average value for the current prefix sum and rounds it up using math.ceil.
+The answer is updated with the maximum of the current answer and the calculated average value.
+"""
+
+
+import math
+from typing import List
+
+
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        # Initialize answer and the prefix sum.
+        answer = 0
+        prefix_sum = 0
+
+        # Iterate over nums, update prefix sum and answer.
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            # Calculate the average value and round up using math.ceil.
+            average_value = math.ceil(prefix_sum / (i + 1))
+            # Update the answer with the maximum of current answer and average value.
+            answer = max(answer, average_value)
+
+        # Return the minimum possible value of the array.
+        return answer
