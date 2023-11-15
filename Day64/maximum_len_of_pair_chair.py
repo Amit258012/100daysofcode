6509ed087c283846232dc1d8688b40d04e5350de
Amit258012 <amit258012@gmail.com>
2023-11-15 20:59:28 +0530

    problem 2

---
+++ Day64/maximum_len_of_pair_chair.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/two-city-scheduling/
+
+"""
+Time complexity:- O(N logN)
+Space Complexity:- O(1) 
+"""
+
+"""
+Intuition:
+
+The code sorts the pairs based on the second element (tail) in ascending order.
+It iterates through the sorted pairs, selecting pairs such that the head is greater than the current tail.
+The length of the longest chain is updated accordingly.
+"""
+
+
+import math
+from typing import List
+
+
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        # Get the length of the pairs.
+        N = len(pairs)
+
+        # Sort the pairs based on the second element (tail) in ascending order.
+        pairs.sort(key=lambda x: x[1])
+
+        # Initialize variables to keep track of the answer and the current tail value.
+        longest_chain = 0
+        current_tail = -math.inf
+
+        # Iterate through the sorted pairs.
+        for head, tail in pairs:
+            # If the head is greater than the current tail, update the current tail and increment the answer.
+            if head > current_tail:
+                current_tail = tail
+                longest_chain += 1
+
+        # Return the length of the longest chain.
+        return longest_chain
